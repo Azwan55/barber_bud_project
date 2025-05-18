c16ffb269f6d111cfe99d3d1ee2a43c94a5b8e5f
Norosazwan <norosazwan10@gmail.com>
2025-05-18 12:50:18 +0800

    fix api issues

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,14 @@
 import Flutter
 import UIKit
 
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDi9SoHuTtrf49Bp8IGRFuqBJLbHwSNSAQ")
+    GMSServices.provideAPIKey("google_api_key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
